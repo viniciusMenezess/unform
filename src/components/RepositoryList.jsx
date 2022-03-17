7b6e58c496facc9856748b3e693c9deefd68c54d
Vinicius Menezes <vini.ads13@outlook.com>
2022-03-17 17:37:34 -0300

    :pencil: almost finishing my first example in REACT

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,23 +1,27 @@
+import {useState, useEffect}from 'react'
 import { RepositoryItem } from "./RepositoryItem"
-
-const repository= {
-  name:'Unform',
-  description:'Forms in React',
-  link:'https://github.com/unform/unform'
-}
+import '../styles/repositories.scss'
 
 export function RepositoryList() {
+  const [repositories,setRepositories] = useState([])
+
+useEffect(() =>{
+  fetch('https://api.github.com/orgs/rocketseat/repos')
+  .then(response => response.json())
+  .then(data => setRepositories(data))
+  .catch(error => console.error(error))
+}, [])
+
   return (
     <section className = "repository-list">
       <h1> Lista de repositórios</h1>
 
 
       <ul>
-      <RepositoryItem repository={repository} />
-      <RepositoryItem repository={repository}/>
-      <RepositoryItem repository={repository}/>
-      <RepositoryItem repository={repository}/>
-      <RepositoryItem repository={repository}/>
+      {repositories.map(repository => {
+        return <RepositoryItem key={repository.name} repository={repository} />
+      })}
+      
       </ul>
 
     </section>
